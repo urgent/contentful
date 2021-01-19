de3c7fe76ed3dbe5ca193a95384b22662c480e2b
steve <steve.hartken@gmail.com>
2021-01-18 23:52:43 -0500

    #2 -- Accept HTTP
    Return contentful response

--- app/index.js
+++ app/index.js
@@ -1,18 +1,25 @@
 import fetch from 'node-fetch';
+import * as Fastify from 'fastify';
 
-const body = {
-    "query": "query{bountyCollection{items{id, title}}}"
-}
+const server = Fastify.fastify({ logger: true });
 
-const response = await fetch('https://graphql.contentful.com/content/v1/spaces/ge2h41wcr1qd', {
-    method: 'post',
-    body: JSON.stringify(body),
-    headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer DXgCHvMuSZokRWL6Ht6UVkBfA7HSkHDE1OPaVB5qd8g'
+server.post('/', {}, async (request, reply) => {
+    const response = await fetch('https://graphql.contentful.com/content/v1/spaces/ge2h41wcr1qd', {
+        method: 'post',
+        body: JSON.stringify(request.body),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer DXgCHvMuSZokRWL6Ht6UVkBfA7HSkHDE1OPaVB5qd8g'
+        }
+    });
+    reply.send(await response.json())
+    return
+})
+
+server.listen(3000, function (err, address) {
+    if (err) {
+        server.log.error(err)
+        process.exit(1)
     }
-});
-
-const json = await response.json();
-
-console.log(json.data.bountyCollection.items);
+    server.log.info(`server listening on ${address}`)
+})
